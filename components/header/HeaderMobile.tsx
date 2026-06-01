c3a66d9c1dc88aa951513319ff600bd7bb44393a
Syed Umer Farooq <farooq2805@gmail.com>
2026-06-01 12:35:30 +0530

    design: increase mobile logo size by 35% to 81px and adjust navbar height

--- components/header/HeaderMobile.tsx
+++ components/header/HeaderMobile.tsx
@@ -167,7 +167,7 @@ const closeAll = () => {
         </Link>
 
         <div className="bg-[#FEE5ED] shadow-sm border-b border-[#f57bb4]/20">
-          <div className="grid grid-cols-3 items-center px-3 h-[85px] relative">
+          <div className="grid grid-cols-3 items-center px-3 h-[96px] relative">
             <div className="flex justify-start">
               <button
                 onClick={() => setMenuOpen(true)}
@@ -185,7 +185,7 @@ const closeAll = () => {
               >
                 <img
                   src="/logo.png"
-                  className="h-[60px] w-auto object-contain block mx-auto mobile-logo"
+                  className="h-[81px] w-auto object-contain block mx-auto mobile-logo"
                   alt="Khatoon Collection"
                   loading="eager"
                 />
