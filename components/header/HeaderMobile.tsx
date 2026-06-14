1129712cc211e11b91183892d1be2a89318482be
Syed Umer Farooq <farooq2805@gmail.com>
2026-06-14 12:28:34 +0530

    style(header): increase mobile logo height by 40% and header bar height for legibility

--- components/header/HeaderMobile.tsx
+++ components/header/HeaderMobile.tsx
@@ -167,7 +167,7 @@ const closeAll = () => {
         </Link>
 
         <div className="bg-[#FEE5ED] shadow-sm border-b border-[#f57bb4]/20">
-          <div className="relative flex items-center justify-between px-3 h-[72px] bg-[#FEE5ED]">
+          <div className="relative flex items-center justify-between px-3 h-[84px] bg-[#FEE5ED]">
             {/* Left Column: Menu Button */}
             <div className="z-10 flex-shrink-0 flex justify-start">
               <button
@@ -187,7 +187,7 @@ const closeAll = () => {
               >
                 <img
                   src="/logo.svg"
-                  className="h-[48px] w-auto block mobile-logo transition-transform duration-300 hover:scale-102"
+                  className="h-[68px] w-auto block mobile-logo transition-transform duration-300 hover:scale-102"
                   style={{ objectFit: 'contain' }}
                   alt="Khatoon Collection"
                   loading="eager"
